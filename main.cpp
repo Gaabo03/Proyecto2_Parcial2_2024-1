300d1ebc132bd4b16bf341fdf1fd424a264e9ea5
Gabriel Ulacio <gabriel.ulacio@unet.edu.ve>
2024-05-23 12:29:30 -0400

    fix: create proyect in dev-c++

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
-void imprimirEnPosicion(COORD posicion, const char *texto)
-{
+#include <iostream>
+#include <windows.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+void imprimirEnPosicion(COORD posicion, const char *texto){
     HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
     SetConsoleCursorPosition(hConsole, posicion);
     std::cout << texto;
+}
+
+int main(int argc, char** argv) {
+
 }
